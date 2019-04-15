bccd4c0bea4619ed1fccd1907029f5cac3e5ddda
anto32795 <37984054+anto32795@users.noreply.github.com>
2019-04-15 11:26:28 +0200

    Update sudoku.py

--- sudoku.py
+++ sudoku.py
@@ -1,9 +1,15 @@
 soluciones_sudoku = 0
 
-caja = [
-    [5, 3, 0],
-    [6, 0, 0],
-    [0, 9, 8]
+tablero_input = [
+    [5, 3, 0, 0, 7, 0, 0, 0, 0],
+    [6, 0, 0, 1, 0, 5, 0, 0, 0],
+    [0, 9, 8, 0, 0, 0, 0, 6, 0],
+    [8, 0, 0, 0, 0, 0, 0, 0, 3],
+    [4, 0, 0, 8, 0, 3, 0, 0, 1],
+    [7, 0, 0, 0, 2, 4, 0, 0, 6],
+    [0, 6, 1, 0, 0, 0, 2, 8, 0],
+    [0, 0, 0, 4, 1, 9, 0, 0, 5],
+    [0, 0, 0, 0, 8, 0, 0, 7, 9]
 ]
 
 
@@ -35,7 +41,7 @@ def obtenCaja(tablero, fila, columna):
     return caja_aux
 
 
-def unicoEnCaja(caja, elem):
+def unicoEnCaja(caja, elem): #falla aqui
     for i in range(0, 3):
         for j in range(0, 3):
             if caja[i][j] == elem:
@@ -44,22 +50,30 @@ def unicoEnCaja(caja, elem):
 
 
 def candidatoEsValido(fila, columna, num_candidato):
-    if fila.__contains__(num_candidato) or columna.__contains__(num_candidato):
-        return False
-    else:
-        return True
+    for i in range(0, 9):
+        if fila[i] == num_candidato:
+            return False
+    for i in range(0, 9):
+        if columna[i] == num_candidato:
+            return False
+    return True
 
 
 def quedanHuecosLibres(tablero):
-    if tablero.__contains__(0):
-        return True
-    else:
-        return False
-def obten_fila(tablero,indice):
+    for i in range(0, 9):
+        for j in range(0, 9):
+            if tablero[i][j] == 0:
+                return True
+    return False
+
+
+def obten_fila(tablero, indice):
     return tablero[indice]
-def obten_columna(tablero,indice):
+
+
+def obten_columna(tablero, indice):
     rtn = []
-    for i in range(0,9):
+    for i in range(0, 9):
         rtn.append(tablero[i][indice])
     return rtn
 
@@ -73,10 +87,17 @@ def resuelveTablero(tablero, soluc_parcial, lista_candidatos):
                 if tablero[i][j] == 0:
                     for k in range(0, len(lista_candidatos)):
                         candidato = lista_candidatos[k]  # generar candidato k-esimo
-                        fila_parametro = obten_fila(tablero,i)
-                        columna_parametro= obten_columna(tablero,j)
-                        if unicoEnCaja(obtenCaja(tablero,i,j), candidato) and candidatoEsValido(fila_parametro,columna_parametro,candidato):
+                        fila_parametro = obten_fila(tablero, i)
+                        columna_parametro = obten_columna(tablero, j)
+                        if unicoEnCaja(obtenCaja(tablero, i, j), candidato) and candidatoEsValido(fila_parametro,
+                                                                                                  columna_parametro,
+                                                                                                  candidato):
                             soluc_parcial[i][j] = candidato
                             resuelveTablero(tablero, soluc_parcial, lista_candidatos)
 
                             soluc_parcial[i][j] = 0
+
+
+sol_p = tablero_input.copy()
+resuelveTablero(tablero_input,sol_p,[1,2,3,4,5,6,7,8,9])
+print(soluciones_sudoku)
